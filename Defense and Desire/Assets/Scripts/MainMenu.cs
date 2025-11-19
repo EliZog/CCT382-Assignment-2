471701604234712b92435b52aea9e8d2db177eee
Nayal-Sher <nayalsher@hotmail.com>
2025-11-19 16:01:21 -0500

    Main Menu
    Options

---
+++ Defense and Desire/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject overlay;
+    public GameObject optionsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        overlay.SetActive(true);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        overlay.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT GAME!");
+    }
+}
